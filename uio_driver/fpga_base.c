9ff3c5dfde014c14201f9d730c67b04be2a09180
Oliver Bründler <oliver.bruendler@enclustra.com>
2020-04-20 10:09:19 +0200

    DEVEL: Clean-up UIO driver

--- uio_driver/fpga_base.c
+++ uio_driver/fpga_base.c
@@ -1,34 +1,46 @@
+//***************************************************************************************
+// Includes
+//***************************************************************************************
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/platform_device.h>
 #include <linux/uio_driver.h>
 
+//***************************************************************************************
+// Module Information
+//***************************************************************************************
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Oliver Bruendler");
 MODULE_DESCRIPTION("fpga_base driver");
 
+
+//***************************************************************************************
+// Implementation
+//***************************************************************************************
+static struct resource uio_resources[] = {
+	[0] = {
+		.name	= "reg",			//Registers
+		.start	= 0x43C10000,		//Start address
+		.end	= 0x43C10FFF,		//End address
+		.flags	= IORESOURCE_MEM,	//Registers are threated as IO memory
+	},
+	[1] = {
+		.name	= "dummy",			//We do not use an IRQ, so let's name th resource "dummy" (it is required, so we cannot omit it)
+		.flags	= IORESOURCE_IRQ,	//This is an IRQ
+	},
+};
+
+//Information about our UIO module
 static struct uio_info info = {
 	.name = "fpga_base",
 	.version = "1.0",
-	.irq = UIO_IRQ_NONE,
+	.irq = UIO_IRQ_NONE,	//We do not handle an IRQ
 };
 
-static struct resource uio_resources[] = {
-	[0] = {
-		.name	= "reg",
-		.start	= 0x43C10000,
-		.end	= 0x43C10FFF,
-		.flags	= IORESOURCE_MEM,
-	},
-	[1] = {
-		.name	= "dummy",
-		.flags	= IORESOURCE_IRQ,
-	},
-};
-
+//Information for registering our driver
 static struct platform_device my_dev = {
-	.name = "uio_pdrv_genirq",
+	.name = "uio_pdrv_genirq",	//Name of the general uio driver 
 	.id = -1,
 	.dev = {
 		.platform_data = &info,
@@ -37,6 +49,9 @@ static struct platform_device my_dev = {
 	.num_resources	= ARRAY_SIZE(uio_resources),
 };
 
+//***************************************************************************************
+// Handling of module insertaion / removal
+//***************************************************************************************
 static int __init uio_init(void)
 {
 	printk(KERN_INFO "fpga_base uio_init()\n");
--- uio_driver/setup.sh
+++
@@ -1,8 +0,0 @@
-pushd ../bsp-xilinx
-export PATH=$PATH:$(pwd)/bin/arm-none-linux-gnueabi-static/bin/
-export ARCH=arm
-export CROSS_COMPILE=arm-none-linux-gnueabi-
-popd
-
-make clean
-make
